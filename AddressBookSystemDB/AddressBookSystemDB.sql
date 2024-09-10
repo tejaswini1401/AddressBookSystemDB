f72c0a0b5f5bd471195e446a188e64e3c10d7ec2
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-10 13:29:07 +0530

    Added: Create Table in DB

--- AddressBookSystemDB/AddressBookSystemDB.sql
+++ AddressBookSystemDB/AddressBookSystemDB.sql
@@ -1,3 +1,17 @@
 create database address_book_system;
 use address_book_system;
 select database();
+
+create table AddressBookSystem(
+id int auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+address varchar(100),
+city varchar(50),
+state varchar(50),
+zip INT,
+phoneNo varchar(12),
+email varchar(100)
+);
+
+describe AddressBookSystem;
